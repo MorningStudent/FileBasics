52faea64f2648031f266710ef3bfdf5bb06c32b6
VictorCorlateanu <126675107+VictorCorlateanu@users.noreply.github.com>
2023-07-16 15:58:04 +0300

    Add files via upload
    This version has added a scanner for reading the values from the file that is eventually created by the user.

--- savingPersonalData/App.java
+++ savingPersonalData/App.java
@@ -7,43 +7,50 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        
         Scanner in = new Scanner(System.in);
-
-        System.out.print("Please enter your name, last name, age and your rating: ");
-        String name     = in.next();
-        String lastName = in.next();
-        String fullName = name + " " + lastName;
-        String age      = in.next();
-        String raiting  = in.next();
-
-        System.out.println();
-        System.out.print("Do you want to delete the information at the end of session? Answer with Yes or No! ");
-        String answer = in.next();
+        String name     = null;
+        String lastName = null;
+        int    age;
+        double raiting;
 
         File file = new File("name.txt");
+
         if(file.exists()) {
+            Scanner fin = new Scanner(file);
             System.out.println("File found!");
+            name     = fin.next();
+            lastName = fin.next();
+            age      = fin.nextInt();
+            raiting  = fin.nextDouble();
         } else {
             System.out.println("File not found!");
             file.createNewFile();
             System.out.println("Such file is created!");
+            System.out.print("Please enter your name, last name, age and your rating: ");
+            name     = in.next();
+            lastName = in.next();
+            age      = in.nextInt();
+            raiting  = in.nextDouble();
+            System.out.println();
+            System.out.print("Do you want to delete the information at the end of session? Answer with Yes or No! ");
+            String answer = in.next();
+            FileWriter fw = new FileWriter(file);                         // A small guess
+            fw.write(name + " " + lastName + " " + age + " "  + raiting); // <- Even though the "age" and "raiting" are primitive
+                                                                          // int and double it still converts it into String?!
+                                                                          // as I saw fw.write can accept only String, char and int...
+            fw.close();
+
+            if(answer.equals("Yes") || answer.equals("yes") || answer.equals("Y") || answer.equals("y")) {
+                if(file.delete()) {
+                    System.out.println("Information deleted!");
+                } else {
+                    System.out.println("Information deleting failed!");
+                };
+            } else if (answer.equals("No") || answer.equals("no") || answer.equals("N") || answer.equals("n"));
+
         }
 
-        System.out.println("Hi " + fullName + "!");
-
-        FileWriter fw = new FileWriter(file);
-        fw.write(fullName + " " + age + " "  + raiting);
-        fw.close();   // <-- when using fw.flush() it keeps the file opened and the Windows locks the file so we cann't delete
-                      // it while it's locked, therefore we need to close all the operators that maitain the file openned
-
-        if(answer.equals("Yes") || answer.equals("yes") || answer.equals("Y") || answer.equals("y")) {
-            if(file.delete()) {
-                System.out.println("Information deleted!");
-            } else {
-                System.out.println("Information deleting failed!");
-            };
-        } else if (answer.equals("No") || answer.equals("no") || answer.equals("N") || answer.equals("n"));
-
+        System.out.println("Hi " + name + " " + lastName  + "!");
     }
+
 }
